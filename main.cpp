34fd878164a3fab5f6188ccc5e4ca91ba84a2e68
chubyrd <chubyrd1@gmail.com>
2021-06-18 12:31:26 -0300

    Cambio el main para usar las nuevas clases

--- main.cpp
+++ main.cpp
@@ -1,38 +1,23 @@
-#include "World.h"
-#include "yaml-cpp/yaml.h"
 #include <fstream>
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include "GameManager.h"
+#include "protocol/Protocol.h"
+#include "EventReceiver.h"
 
 int main(){
-    YAML::Node yaml_mapa = YAML::LoadFile("mapa.yaml");
+    Socket acceptador(NULL, "8080", true);
+    Protocol protocol(acceptador.accept_one());
+    GameManager gm("mapa.yaml");
+    EventReceiver receiver(gm, protocol);
 
-    std::vector<int> size = yaml_mapa["size"].as<std::vector<int>>();
-    std::cout << "Creating world of size (" << size[0] << ", " << size[1] << ")\n";
-    World world(size[0], size[1]);
-
-    std::vector<int> spawn = yaml_mapa["spawn"].as<std::vector<int>>();
-    std::cout << "Creating player in (" << spawn[0] << ", " << spawn[1] << ")\n";
-    Player p = world.createPlayer(spawn[0], spawn[1]);
-
-
-    YAML::Node boxes = yaml_mapa["boxes"];
-    std::cout << "Adding boxes in:\n";
-    for (YAML::iterator it = boxes.begin(); it != boxes.end(); ++it) {
-        std::vector<int> box = it->as<std::vector<int>>();
-        std::cout << "\t- [" << box[0] << ", " << box[1] <<"]\n";
-        world.addBox(box[0], box[1]);
-    }
-
-    p.applyImpulse(1, 1);
-
-    for (int i = 0; i < 25; i++){
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        world.step(0.5);
-        b2Vec2 position = p.getPosition();
-        printf("Jugador:\n\tx:%4.2f\n\ty:%4.2f\n", position.x, position.y); 
-    }  
+    // for (int i = 0; i < 25; i++){
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    //     world.step(0.5);
+    //     b2Vec2 position = p.getPosition();
+    //     printf("Jugador:\n\tx:%4.2f\n\ty:%4.2f\n", position.x, position.y); 
+    // }  
 
     return 0;
 }
