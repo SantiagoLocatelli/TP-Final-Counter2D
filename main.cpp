ff0395ac61b64ace88d14a694afa12903e02d82d
chubyrd <chubyrd1@gmail.com>
2021-06-19 16:58:13 -0300

    Ultimos cambios al main

--- main.cpp
+++ main.cpp
@@ -12,12 +12,13 @@ int main(){
     GameManager gm("mapa.yaml");
     EventReceiver receiver(gm, protocol);
 
-    // for (int i = 0; i < 25; i++){
-    //     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    //     world.step(0.5);
-    //     b2Vec2 position = p.getPosition();
-    //     printf("Jugador:\n\tx:%4.2f\n\ty:%4.2f\n", position.x, position.y); 
-    // }  
+    while (true){
+        std::this_thread::sleep_for(std::chrono::milliseconds(42));
+        gm.step_world(0.042);
+        float x, y;
+        gm.get_player_position(x, y);
+        protocol.send_position(x, y);
+    }  
 
     return 0;
 }
