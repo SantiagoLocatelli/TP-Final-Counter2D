05a9357db377bd033949fb37db601fa0acecdd52
chubyrd <chubyrd1@gmail.com>
2021-06-24 19:48:22 -0300

    Agrego métodos para mandar y recibir el modelo

--- protocol/Protocol.h
+++ protocol/Protocol.h
@@ -3,6 +3,8 @@
 
 #include "Socket.h"
 #include <stdint.h>
+#include "MapInfo.h"
+#include "ModelInfo.h"
 #define DEC_SENT 2
 
 enum Event : char {UP_ON, UP_OFF, DOWN_ON, DOWN_OFF, RIGHT_ON, RIGHT_OFF
@@ -13,24 +15,28 @@ class Protocol{
         Socket skt;
         
         //Envía un short por el socket. 
-        void send_short(const uint16_t buf);
+        void send_short(const uint16_t &buf);
         //Recibe un short por el socket.
         void recv_short(uint16_t &buf);
 
-        void send_byte(const char byte);
+        void send_byte(const char &byte);
         void recv_byte(char& byte);
-
-    public:
-        explicit Protocol(Socket skt);
-
-        void recv_window_size(int& w, int& h);
-        void recv_pixel_position(int& x, int& y);
 
         //Envía una posicion por el socket. Solo se envían los primeros 
         //DEC_SENT decimales, el resto se pierde.
-        void send_position(const float x, const float y);
+        void send_position(const float &x, const float &y);
         //Recibe una posicion por el socket.
         void recv_position(float &x, float &y);
+    
+    public:
+        explicit Protocol(Socket skt);
+
+        void send_map_info(const MapInfo &mapInfo);
+        void recv_map_info(MapInfo &mapInfo);
+
+        void send_model_info(const ModelInfo &modelInfo);
+        void recv_model_info(ModelInfo &modelInfo);
+
 
         //Envía un evento por el socket.
         void send_event(const Event event);
