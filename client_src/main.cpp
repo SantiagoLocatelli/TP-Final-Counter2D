120160d5f3b7e68207dfac526b6a8870a62e07ee
chubyrd <chubyrd1@gmail.com>
2021-07-02 17:52:08 -0300

    Arreglo conflicto

--- client_src/main.cpp
+++ client_src/main.cpp
@@ -2,23 +2,6 @@
 #include "../common_src/Stopwatch.h"
 #include "GameManager.h"
 
-<<<<<<< HEAD
-#define PIXELS_PER_METER 100
-
-void renderBoxes(std::list<Prot_Box> boxes, SdlTexture& boxTexture, Camera& cam) {
-    for(auto it = boxes.begin(); it != boxes.end(); it++){
-        boxTexture.render(it->x*PIXELS_PER_METER - cam.getPosX(), it->y*PIXELS_PER_METER - cam.getPosY(), PIXELS_PER_METER, PIXELS_PER_METER);
-    }
-}
-
-void renderPlayers(ModelInfo model, SdlTexture& playerTexture, Camera& cam) {
-    printf("cantidad de jugadores: %i\n", model.players.size());
-    for( auto it = model.players.begin(); it != model.players.end(); it++) {
-        playerTexture.render(it->x - cam.getPosX(), it->y - cam.getPosY(), PIXELS_PER_METER, PIXELS_PER_METER, it->angle);
-    }
-}
-=======
->>>>>>> develop
 
 int main(int argc, char* argv[]){
 
