7267cf376d0291ac8585dd1f24e5291ff5a57528
chubyrd <chubyrd1@gmail.com>
2021-07-04 15:44:19 -0300

    Agrego clase CollisionHandler

---
+++ server_src/game_model/CollisionHandler.h
@@ -0,0 +1,11 @@
+#ifndef _COLLISION_HANDLER_H_
+#define _COLLISION_HANDLER_H_
+
+#include <box2d/box2d.h>
+
+class CollisionHandler : public b2ContactListener{
+    public:
+        void BeginContact(b2Contact* contact) override;
+};
+
+#endif
