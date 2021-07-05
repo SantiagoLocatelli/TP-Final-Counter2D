f5686fe709b7efa51218bed4fd950b71a3ba6067
chubyrd <chubyrd1@gmail.com>
2021-07-05 19:45:18 -0300

    Majeo de excepciones en el cliente

--- client_src/Events/eventManager.cpp
+++ client_src/Events/eventManager.cpp
@@ -1,78 +1,82 @@
 #include "eventManager.h"
 #include "gameMath.h"
+#include <iostream>
 
 EventManager::EventManager(Protocol& com, bool& quit, GameManager& game):com(com), quit(quit), game(game){}
 
 void EventManager::run(){
+    try{
+        SDL_Event e;
+        while (!quit && SDL_WaitEvent(&e) != 0){
+            Event event;
+            switch (e.type) {
+                case SDL_QUIT:
+                    this->quit = true; 
+                    break;
 
-    SDL_Event e;
-    while (!quit && SDL_WaitEvent(&e) != 0){
-        Event event;
-        switch (e.type) {
-            case SDL_QUIT:
-                this->quit = true; 
-                break;
+                //Ambos casos hacen lo mismo
+                case SDL_KEYUP:
+                case SDL_KEYDOWN:
+                    if (e.key.repeat == 0){
+                        switch ( e.key.keysym.sym ){
+                            case SDLK_w: 
+                                event.type = TOGGLE_MOVEMENT;
+                                event.info.dir = UP; 
+                                break;
+                            case SDLK_s: 
+                                event.type = TOGGLE_MOVEMENT;
+                                event.info.dir = DOWN; 
+                                break;
+                            case SDLK_a: 
+                                event.type = TOGGLE_MOVEMENT;
+                                event.info.dir = LEFT; 
+                                break;
+                            case SDLK_d: 
+                                event.type = TOGGLE_MOVEMENT;
+                                event.info.dir = RIGHT; 
+                                break;
+                            case SDLK_e: /*plantar bomba*/break;
+                            case SDLK_r: /*recargar*/break;
+                            case SDLK_1: /*cambiar arma a 1*/break;
+                            case SDLK_2: /*cambiar arma a 2*/break;
+                            case SDLK_3: /*cambiar arma a 3*/break;
+                            case SDLK_4: /*cambiar arma a 4*/break;
+                            case SDLK_g: /*dropear arma*/break;
+                        }
 
-            //Ambos casos hacen lo mismo
-            case SDL_KEYUP:
-            case SDL_KEYDOWN:
-                if (e.key.repeat == 0){
-                    switch ( e.key.keysym.sym ){
-                        case SDLK_w: 
-                            event.type = TOGGLE_MOVEMENT;
-                            event.info.dir = UP; 
-                            break;
-                        case SDLK_s: 
-                            event.type = TOGGLE_MOVEMENT;
-                            event.info.dir = DOWN; 
-                            break;
-                        case SDLK_a: 
-                            event.type = TOGGLE_MOVEMENT;
-                            event.info.dir = LEFT; 
-                            break;
-                        case SDLK_d: 
-                            event.type = TOGGLE_MOVEMENT;
-                            event.info.dir = RIGHT; 
-                            break;
-                        case SDLK_e: /*plantar bomba*/break;
-                        case SDLK_r: /*recargar*/break;
-                        case SDLK_1: /*cambiar arma a 1*/break;
-                        case SDLK_2: /*cambiar arma a 2*/break;
-                        case SDLK_3: /*cambiar arma a 3*/break;
-                        case SDLK_4: /*cambiar arma a 4*/break;
-                        case SDLK_g: /*dropear arma*/break;
+                        this->com.send_event(event);
                     }
+                    break; 
 
+                /**<SDL_MouseButtonEvent ::SDL_MOUSEBUTTONDOWN or ::SDL_MOUSEBUTTONUP */
+                //case SDL_MOUSEBUTTONUP: Por ahora solo cuando apreta el boton 
+                case SDL_MOUSEBUTTONDOWN:
+                    switch (e.button.button){
+                        case SDL_BUTTON_LEFT:
+                            event.type = TOGGLE_WEAPON;
+                            break;
+                        case SDL_BUTTON_RIGHT:
+                            // apuntar con mira o cambiar modo
+                            break;
+                        case SDL_BUTTON_MIDDLE:
+                            // cualquier que se les ocurra
+                            break;
+                    }
                     this->com.send_event(event);
-                }
-                break; 
-
-            /**<SDL_MouseButtonEvent ::SDL_MOUSEBUTTONDOWN or ::SDL_MOUSEBUTTONUP */
-            //case SDL_MOUSEBUTTONUP: Por ahora solo cuando apreta el boton 
-            case SDL_MOUSEBUTTONDOWN:
-                switch (e.button.button){
-                    case SDL_BUTTON_LEFT:
-                        event.type = TOGGLE_WEAPON;
-                        break;
-                    case SDL_BUTTON_RIGHT:
-                        // apuntar con mira o cambiar modo
-                        break;
-                    case SDL_BUTTON_MIDDLE:
-                        // cualquier que se les ocurra
-                        break;
-                }
-                this->com.send_event(event);
-                break;
-            case SDL_MOUSEMOTION:
-                float radians = Math::calculateRadians({this->game.getRelativePlayerPosX(), this->game.getRelativePlayerPosY()}, {e.motion.x, e.motion.y});
-                game.setCrossHair(e.motion.x, e.motion.y);
-                event.type = SET_ANGLE;
-                event.info.angle = radians;
-                this->com.send_event(event);
+                    break;
+                case SDL_MOUSEMOTION:
+                    float radians = Math::calculateRadians({this->game.getRelativePlayerPosX(), this->game.getRelativePlayerPosY()}, {e.motion.x, e.motion.y});
+                    game.setCrossHair(e.motion.x, e.motion.y);
+                    event.type = SET_ANGLE;
+                    event.info.angle = radians;
+                    this->com.send_event(event);
+            }
         }
-    }
 
-    com.close();
+        com.close();
+    } catch (const std::exception &e){
+        std::cerr << "ERROR en `EventManager`: " << e.what() << std::endl;
+    }
 }
 
 void EventManager::stop(){
--- client_src/main.cpp
+++ client_src/main.cpp
@@ -1,38 +1,42 @@
 #include "Events/eventManager.h"
 #include "../common_src/Stopwatch.h"
 #include "GameManager.h"
+#include <iostream>
 
 
 int main(int argc, char* argv[]){
+    try{
+        Protocol server(Socket("localhost", argv[1], false));
+        int window_w = 1100, window_h = 1100;
 
-    Protocol server(Socket("localhost", argv[1], false));
-    int window_w = 400, window_h = 400;
+        MapInfo map;
+        server.recv_map_info(map);
 
-    MapInfo map;
-    server.recv_map_info(map);
-
-    ModelInfo model;
-    server.recv_model_info(model);
-
-    GameManager game(map, model, window_w, window_h);
-
-    bool quit = false;
-    EventManager eventManager(server, quit, game);
-    eventManager.start();
-    Stopwatch stopwatch;
-
-    while (!quit && !model.game_ended) {
-        stopwatch.start();
+        ModelInfo model;
         server.recv_model_info(model);
-        game.update(model);
-        game.render();
-        while (stopwatch.msPassed() < 33) {
-            // le mandas mecha
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        GameManager game(map, model, window_w, window_h);
+
+        bool quit = false;
+        EventManager eventManager(server, quit, game);
+        eventManager.start();
+        Stopwatch stopwatch;
+
+        while (!quit && !model.game_ended) {
+            stopwatch.start();
+            server.recv_model_info(model);
+            game.update(model);
+            game.render();
+            while (stopwatch.msPassed() < 33) {
+                // le mandas mecha
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
         }
+        
+        eventManager.stop();
+        eventManager.join();
+    } catch (const std::exception &e){
+        std::cerr << "ERROR:" << e.what() << std::endl;
     }
-    
-    eventManager.stop();
-    eventManager.join();
 	return 0;
 }
