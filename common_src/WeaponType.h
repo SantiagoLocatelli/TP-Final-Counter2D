3f86bba9d47558002eeece54808705f4c9003409
chubyrd <chubyrd1@gmail.com>
2021-07-07 14:56:41 -0300

    Agrego nuevos tipos de arma

--- common_src/WeaponType.h
+++ common_src/WeaponType.h
@@ -1,6 +1,6 @@
 #ifndef _WEAPON_TYPE_H_
 #define _WEAPON_TYPE_H_
 
-enum WeaponType : char {KNIFE, PISTOL, RIFLE, SNIPER,GOD_GUN};
+enum WeaponType : char {KNIFE, PISTOL, SHOTGUN, RIFLE, SNIPER, BOMB};
 
 #endif
