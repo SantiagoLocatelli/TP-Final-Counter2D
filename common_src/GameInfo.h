cd9524d52e1feefcd71c71f6c7bb350848efc41c
chubyrd <chubyrd1@gmail.com>
2021-07-08 11:52:38 -0300

    Cambio a GameInfo

--- common_src/GameInfo.h
+++ common_src/GameInfo.h
@@ -1,10 +1,13 @@
 #ifndef _GAME_INFO_H_
 #define _GAME_INFO_H_
 
+#include <stdint.h>
+
 struct GameInfo{
     char name[30];
     char map[30];
-    int players;
+    uint16_t players;
+    uint16_t max_players;
 };
 
 #endif
