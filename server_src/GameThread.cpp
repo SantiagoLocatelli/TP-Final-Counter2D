b59fc22c242187aaaefe83b1d3b2f167f1aa9f7b
chubyrd <chubyrd1@gmail.com>
2021-07-08 00:38:55 -0300

    Agrego clase GameThread

---
+++ server_src/GameThread.cpp
@@ -0,0 +1,56 @@
+#include "GameThread.h"
+#include <utility>
+#include "../common_src/Stopwatch.h"
+
+#include <iostream>
+#include <chrono>
+
+GameThread::GameThread(char map[]):game(map), eventHandler(game), gameEnded(false), playerNumber(0){}
+
+void GameThread::addPlayer(Protocol protocol){
+    clients.push_back(new ClientManager(std::move(protocol), queue, emitter, playerNumber));
+    playerNumber++;
+    game.createPlayer();
+}
+
+bool GameThread::ended(){
+    return gameEnded;
+}
+
+void GameThread::run(){
+    for (auto cli: clients){
+        cli->start();
+    }
+    try{
+        
+        Stopwatch stopwatch;
+
+        emitter.emitMap(game.getMapInfo());
+
+        do{
+            stopwatch.start();
+            while (stopwatch.msPassed() < 33){
+                //TODO: Sacar este busy wait
+                if (!queue.isEmpty()){
+                    int id;
+                    Event event = queue.pop(id);
+                    eventHandler.executeEvent(event, id);
+                } else {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                }
+            }
+
+            game.step();
+            emitter.emitModel(std::move(game.getModelInfo()));
+            game.clearFrameEvents();
+        } while (!game.ended());
+    } catch (const std::exception &e){
+        std::cout << "ERROR: " << e.what() << std::endl;
+    }
+    
+    gameEnded = true;
+    for (ClientManager *cli: clients){
+        cli->join();
+        delete cli;
+    }
+}
--- server_src/GameThread.h
+++ server_src/GameThread.h
@@ -3,10 +3,28 @@
 
 #include "../common_src/Thread.h"
 #include "GameProxy.h"
+#include "ClientManager.h"
+#include "EventHandler.h"
+#include "Emitter.h"
+#include "EventQueue.h"
+#include <list>
 
 class GameThread: public Thread{
+    private:
+        GameProxy game;
+        std::list<ClientManager*> clients;
+        Emitter emitter;
+        EventQueue queue;
+        EventHandler eventHandler;
+        bool gameEnded;
+        int playerNumber;
+
+        void run();
+    
     public:
-        GameProxy &game;
+        explicit GameThread(char map[]);
+        void addPlayer(Protocol protocol);
+        bool ended();
 }
 
 #endif
