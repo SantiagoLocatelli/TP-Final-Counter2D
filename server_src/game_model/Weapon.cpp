b09a0cdda39665c200f61216b0fad27f6112688a
chubyrd <chubyrd1@gmail.com>
2021-07-09 12:56:03 -0300

    Cambio interfáz de Weapon

--- server_src/game_model/Weapon.cpp
+++ server_src/game_model/Weapon.cpp
@@ -1,7 +1,7 @@
 #include "Weapon.h"
+#include <cstdlib>
 
-Weapon::Weapon(World *world, float damage, WeaponType type)
-:owner(nullptr), world(world), damage(damage), type(type){}
+Weapon::Weapon(World *world, float maxDamage, float minDamage, float spread, float damageFalloff, WeaponType type):owner(nullptr), world(world), maxDamage(maxDamage), minDamage(minDamage), spread(spread), damageFalloff(damageFalloff), type(type){}
 
 
 void Weapon::changeOwner(Player *newOwner){
@@ -12,8 +12,12 @@ World *Weapon::getWorld(){
     return world;
 }
 
-void Weapon::shootBullet(float spread){
+void Weapon::shootBullet(){
     Ray ray;
+
+    //Le agrego un desvio a la bala
+    float r = ((float) rand()) / (float) RAND_MAX;
+    float spread = (2*r-1)*(this->spread);
     std::array<float, 2> pos = owner->getPosition();
     ray.x = pos[0];
     ray.y = pos[1];
@@ -29,5 +33,19 @@ void Weapon::shootBullet(float spread){
 
 WeaponType Weapon::getType(){
     return type;
+}
+
+void Weapon::toggle(){
+    shootBullet();
+}
+
+float Weapon::calculateDamage(float distance){
+    //Calculo el daño entre el min y max
+    float r = ((float) rand()) / (float) RAND_MAX;
+    float damage = minDamage + (r * (maxDamage - minDamage));
+    //El daño disminuye con la distancia
+    damage *= 1/(distance*damageFalloff+1);
+
+    return damage;
 }
 
--- server_src/game_model/Weapon.h
+++ server_src/game_model/Weapon.h
@@ -12,17 +12,21 @@ class Weapon{
     protected:
         Player *owner;
         World *world;
-        float damage;
+        float maxDamage;
+        float minDamage;
+        float spread;
+        float damageFalloff;
+
         WeaponType type;
 
-        virtual float calculateDamage(float distance) = 0;
+        virtual float calculateDamage(float distance);
     public:
-        Weapon(World *world, float damage, WeaponType type);
+        Weapon(World *world, float maxDamage, float minDamage, float spread, float damageFalloff, WeaponType type);
         void changeOwner(Player *newOwner);
         World *getWorld();
         WeaponType getType();
-        virtual void toggle() = 0;
-        void shootBullet(float spread);
+        virtual void toggle();
+        void shootBullet();
         virtual ~Weapon() = default;
 };
 
