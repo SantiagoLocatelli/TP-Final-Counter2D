abefb262a8ff1edc6e2b9628b54ca158fc9f5293
chubyrd <chubyrd1@gmail.com>
2021-07-08 23:34:31 -0300

    Ahora el server usa los nuevos yaml

--- server_src/WorldParser.cpp
+++ server_src/WorldParser.cpp
@@ -1,24 +1,30 @@
 #include "WorldParser.h"
+#include "../common_src/TextureMap.h"
 #include <vector>
 
 WorldParser::WorldParser(const std::string &yamlPath)
 :mapYaml(YAML::LoadFile(yamlPath)){}
 
 void WorldParser::get_size(uint16_t &length, uint16_t &height){
-    std::vector<int> size = mapYaml["size"].as<std::vector<int>>();
+    std::vector<int> size = mapYaml["mapSize"].as<std::vector<int>>();
     length = size[0];
     height = size[1];
 }
 
 std::list<ProtBox> WorldParser::get_boxes(){
+    TextureMap textureMap;
     std::list<ProtBox> boxList;
-    YAML::Node boxes = mapYaml["boxes"];
-    for (YAML::iterator it = boxes.begin(); it != boxes.end(); ++it) {
-        ProtBox b;
-        std::vector<int> pos = it->as<std::vector<int>>();
-        b.x = pos[0];
-        b.y = pos[1];
-        boxList.push_back(b);
+    std::vector<int> map = mapYaml["map"].as<std::vector<int>>();
+    uint16_t height, length;
+    get_size(length, height);
+    
+    for (size_t i = 0; i < map.size(); i++){
+        if (textureMap[map[i]].isBox){
+            ProtBox b;
+            b.x = i%length;
+            b.y = i/height;
+            boxList.push_back(b);
+        }
     }
 
     return boxList;
