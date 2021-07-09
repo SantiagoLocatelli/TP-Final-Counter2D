f56040189a0850da2b5e41c029c188d0538a7db2
chubyrd <chubyrd1@gmail.com>
2021-07-09 13:41:55 -0300

    Agrego clase GameConfig

---
+++ server_src/game_model/GameConfig.cpp
@@ -0,0 +1,32 @@
+#include "GameConfig.h"
+
+GameConfig::GameConfig():configFile(YAML::LoadFile("../../server_src/game_model/GameConfig.yaml")){}
+
+std::map<std::string, float> GameConfig::getWeapon(WeaponType type){
+    std::string name;
+    switch (type){
+    case KNIFE:
+        name = "Knife";
+        break;
+    case PISTOL:
+        name = "Pistol";
+        break;
+    case SHOTGUN:
+        name = "Shotgun";
+        break;
+    case RIFLE:
+        name = "Rifle";
+        break;
+    case SNIPER:
+        name = "Sniper";
+        break;
+    default:
+        name = ""; //No estan en el archivo todavia
+    }
+
+    return configFile["Weapons"][name].as<std::map<std::string, float>>();
+}
+
+std::map<std::string, float> GameConfig::getPlayer(){
+    return configFile["Player"].as<std::map<std::string, float>>();
+}
---
+++ server_src/game_model/GameConfig.h
@@ -0,0 +1,19 @@
+#ifndef _GAME_CONFIG_H_
+#define _GAME_CONFIG_H_
+
+#include "../../common_src/WeaponType.h"
+#include "yaml-cpp/yaml.h"
+#include <string>
+#include <map>
+
+class GameConfig{
+    private:
+        YAML::Node configFile;
+
+    public:
+        GameConfig();
+        std::map<std::string, float> getWeapon(WeaponType type);
+        std::map<std::string, float> getPlayer();
+};
+
+#endif
