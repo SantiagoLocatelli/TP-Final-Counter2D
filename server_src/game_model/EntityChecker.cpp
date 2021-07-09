8c2d5759e1ddb464e480fe79009e7cfb52ac91ce
chubyrd <chubyrd1@gmail.com>
2021-07-09 16:31:40 -0300

    Agrego funcionalidad al EntityChecker

--- server_src/game_model/EntityChecker.cpp
+++ server_src/game_model/EntityChecker.cpp
@@ -5,7 +5,10 @@ EntityChecker::EntityChecker(b2World &world):world(world), hasEntities(false){}
 
 bool EntityChecker::ReportFixture(b2Fixture *fixture){
     hasEntities = true;
-    return false;
+    if (fixture->GetBody()->GetUserData().pointer){
+        hit.push_back((Hittable*)fixture->GetBody()->GetUserData().pointer);
+    }
+    return true;
 }
 
 bool EntityChecker::areaHasEntities(b2Vec2 p1, b2Vec2 p2){
@@ -17,4 +20,14 @@ bool EntityChecker::areaHasEntities(b2Vec2 p1, b2Vec2 p2){
 
     world.QueryAABB(this, aabb);
     return hasEntities;
+}
+
+std::list<Hittable*> &EntityChecker::getHittableInArea(b2Vec2 p1, b2Vec2 p2){
+    b2AABB aabb;
+    aabb.lowerBound = b2Vec2(std::min(p1.x,p2.x), std::min(p1.y,p2.y));
+    aabb.upperBound = b2Vec2(std::max(p1.x,p2.x), std::max(p1.y,p2.y));
+
+    hit.clear();
+    world.QueryAABB(this, aabb);
+    return hit;
 }
--- server_src/game_model/EntityChecker.h
+++ server_src/game_model/EntityChecker.h
@@ -2,15 +2,20 @@
 #define _ENTITY_CHECKER_H_
 #include <box2d/box2d.h>
 
+#include "Hittable.h"
+#include <list>
+
 
 class EntityChecker: public b2QueryCallback{
     private:
         b2World &world;
         bool hasEntities;
+        std::list<Hittable*> hit;
         bool ReportFixture(b2Fixture *fixture) override;
     public:
         explicit EntityChecker(b2World &world);
         bool areaHasEntities(b2Vec2 p1, b2Vec2 p2);
+        std::list<Hittable*> &getHittableInArea(b2Vec2 p1, b2Vec2 p2);
 };
 
 #endif
