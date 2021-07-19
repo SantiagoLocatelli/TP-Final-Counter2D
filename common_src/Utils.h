2a6c21f023442adef4f9bbc9871c1ea3267f5eaf
chubyrd <chubyrd1@gmail.com>
2021-07-19 11:33:02 -0300

    Arreglo en utils

--- common_src/Utils.h
+++ common_src/Utils.h
@@ -2,13 +2,13 @@
 #define _UTILS_H_
 
 #include <cstdint>
-enum SkinType : char {CT1, CT2, CT3, CT4, TT1, TT2, TT3, TT4};
+enum SkinType : uint8_t {CT1, CT2, CT3, CT4, TT1, TT2, TT3, TT4};
 
 enum WeaponType : char {KNIFE, PISTOL, SHOTGUN, RIFLE, SNIPER, BOMB, NO_WEAPON};
 
 enum RoundState : char {BUY, MIDDLE, END};
 
-enum GameState{PLAYING, T_WON, CT_WON, TIE};
+enum GameState : char{PLAYING, T_WON, CT_WON, TIE};
 
 enum RoundResult : char {T_DEAD, BOMB_DEFUSED, TIME_ENDED, /*CT WIN*/
                        CT_DEAD, BOMB_EXPLODED}; /*T WIN*/
